196ea8581879986bd0c85c447759a395a9514b20
Tran Duy Tien <54745802+duytienkh@users.noreply.github.com>
2020-12-30 15:53:37 +0700

    fixed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
+#include <queue>
 using namespace std;
 
 void readGraph(char* inputPath, vector<vector<int>>& graph, int& n){
@@ -31,12 +33,8 @@ bool HPath_DFS(int vertex, vector<vector<int>> graph, int n, vector<bool>& visit
     return result;
 }
 void HPath(vector<vector<int>> graph, int n){
-    bool result = false;
     vector<bool> visited(n, 0);
-    for (int i = 0; i < n; i++){
-        result = HPath_DFS(i, graph, n, visited, n - 1);
-        if (result) break;
-    }
+    bool result = HPath_DFS(0, graph, n, visited, n - 1);
     if (result) cout << "Yes\n";
     else cout << "No\n";
 };
@@ -58,16 +56,53 @@ bool HCycle_DFS(int vertex, int source, vector<vector<int>> graph, int n, vector
 }
 
 void HCycle(vector<vector<int>> graph, int n){
-    bool result = false;
     vector<bool> visited(n, 0);
-    for (int i = 0; i < n; i++){
-        result = HCycle_DFS(i, i, graph, n, visited, n - 1);
-        if (result) break;
-    }
+    bool result = HCycle_DFS(0, 0, graph, n, visited, n - 1);
     if (result) cout << "Yes\n";
     else cout << "No\n";
 };
-void TSP(){};
+
+void TSP_DFS(int vertex, int source, int cost, int& minCost, stack<int>& trace, stack<int>& minPath, vector<vector<int>> graph, int n, vector<bool>& visited, int remainVertices){
+    trace.push(vertex);
+    if (!remainVertices){
+        if (graph[vertex][source] != -1){
+            if (minCost > cost + graph[vertex][source]){
+                minCost = cost + graph[vertex][source];
+                minPath = trace;
+            }
+        }
+    }
+    visited[vertex] = 1;
+    for (int i = 0; i < n; i++){
+        if (!visited[i] && graph[vertex][i] != -1){ // đỉnh i chưa được thăm và có cung giữa đỉnh vertex và i
+            TSP_DFS(i, source, cost + graph[vertex][i], minCost, trace, minPath, graph, n, visited, remainVertices - 1);
+        }
+    }
+    visited[vertex] = 0;
+    trace.pop();
+}
+
+void TSP(vector<vector<int>> graph, int n){
+    vector<bool> visited(n, 0);
+    int minCost = INT_MAX;
+    stack<int> trace, minPath;
+    TSP_DFS(0, 0, 0, minCost, trace, minPath, graph, n, visited, n - 1);
+
+    if (!minPath.empty()){
+        stack<int> s;
+        while (!minPath.empty()){
+            s.push(minPath.top());
+            minPath.pop();
+        }
+
+        while (!s.empty()){
+            cout << s.top() + 1 << ' ';
+            s.pop();
+        }
+        cout << endl << minCost;
+    }
+    else cout << -1;
+};
 
 int main(int argc, char** argv){
     vector<vector<int>> graph;
@@ -77,5 +112,5 @@ int main(int argc, char** argv){
     string action(argv[1]);
     if (action == "-HPath") HPath(graph, n);
     if (action == "-HCycle") HCycle(graph, n);
-    if (action == "-TSP") TSP();
+    if (action == "-TSP") TSP(graph, n);
 }
